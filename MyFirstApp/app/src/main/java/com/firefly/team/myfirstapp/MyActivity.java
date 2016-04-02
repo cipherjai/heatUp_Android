8a40d95a924040f103edc36bbe793f461705daeb
cipherjai <jaiatmart@gmail.com>
2016-04-02 12:15:45 +0530

    Added Button.

--- MyFirstApp/app/src/main/java/com/firefly/team/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/firefly/team/myfirstapp/MyActivity.java
@@ -1,5 +1,6 @@
 package com.firefly.team.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -26,6 +29,12 @@ public class MyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
     }
 
     @Override
